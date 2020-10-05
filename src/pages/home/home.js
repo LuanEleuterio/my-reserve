6d833be02dcf82a07aa01cc0351d6429a356619c
LuanEleuterio <luaneleuterio3@hotmail.com>
2020-10-05 14:07:04 -0300

    Adicionado injeção de html por JS dos elementos restaurantes, introduzido limitação de exibição de caracteres dos nomes dos restaurantes

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,6 +1,100 @@
-
+const conteinerRestaurantes = document.querySelector('.conteudo-restaurantes')
 
 function carregaRestaurantes() {
+    let restaurenteContainer;
+    let restaurantes;
+    let fotoRestaurante;
+    let imgRestaurante;
+    let infoRestaurante;
+    let nomeRestaurante;
+    let categoriaInfo;
+    let iconCategoria;
+    let nomeCategoria;
+    let distanceInfo;
+    let iconDistance;
+    let distancia;
+    let infoNumeroPessoas;
+    let numeroPessoas;
+    let qtdPessoas;
+    let iconPessoas;
+
+    const allRestaurantes = listaRestaurantes;
+
+    console.log(listaRestaurantes);
+    console.log(allRestaurantes)
+
+    allRestaurantes.forEach(values => {
+        restaurenteContainer = document.createElement("a");
+        restaurantes = document.createElement("div");
+
+        fotoRestaurante = document.createElement("div");
+        imgRestaurante = document.createElement("img");
+
+        infoRestaurante = document.createElement("div");
+        nomeRestaurante = document.createElement("p");
+
+        categoriaInfo = document.createElement("div");
+        iconCategoria = document.createElement("i");
+        nomeCategoria = document.createElement("p");
+
+        distanceInfo = document.createElement("div");
+        iconDistance = document.createElement("i");
+        distancia = document.createElement("p");
+
+        infoNumeroPessoas = document.createElement("div");
+        numeroPessoas = document.createElement("div");
+        qtdPessoas = document.createElement("p");
+        iconPessoas = document.createElement("i");
+
+        restaurenteContainer.setAttribute("class", "restaurante-container");
+        restaurantes.setAttribute("class", "restaurantes");
+
+        fotoRestaurante.setAttribute("class", "foto-restaurante");
+        imgRestaurante.setAttribute("src", `${values.img_url}`);
+        console.log(values.img_url)
+        infoRestaurante.setAttribute("class", "info-restaurante");
+        nomeRestaurante.setAttribute("class", "nome-restaurante infos");
+        nomeRestaurante.textContent = values.name;
+
+        categoriaInfo.setAttribute("class", "categoria infos");
+        iconCategoria.setAttribute("class", "fas fa-utensils col-1");
+        nomeCategoria.setAttribute("class", "categoria")
+        nomeCategoria.textContent = values.category;
+
+        distanceInfo.setAttribute("class", "distance infos")
+        iconDistance.setAttribute("class", "fas fa-route col-1");
+        distancia.setAttribute("class", "distancia")
+        distancia.textContent = '2km';
+
+        infoNumeroPessoas.setAttribute("class", "info-num-pessoas");
+        numeroPessoas.setAttribute("class", "numero-pessoas");
+        qtdPessoas.setAttribute("class", "quantidade-pessoa");
+        iconPessoas.setAttribute("class", "fas fa-user-friends icon-pessoa");
+        qtdPessoas.textContent = values.totalPessoas;
+
+        restaurenteContainer.appendChild(restaurantes);
+        restaurantes.appendChild(fotoRestaurante);
+        fotoRestaurante.appendChild(imgRestaurante);
+
+        restaurantes.appendChild(infoRestaurante);
+        infoRestaurante.appendChild(nomeRestaurante);
+        infoRestaurante.appendChild(categoriaInfo);
+        categoriaInfo.appendChild(iconCategoria);
+        categoriaInfo.appendChild(nomeCategoria);
+
+        infoRestaurante.appendChild(distanceInfo);
+        distanceInfo.appendChild(iconDistance);
+        distanceInfo.appendChild(distancia);
+
+        restaurantes.appendChild(infoNumeroPessoas);
+        infoNumeroPessoas.appendChild(numeroPessoas);
+        numeroPessoas.appendChild(qtdPessoas);
+        numeroPessoas.appendChild(iconPessoas);
+
+        conteinerRestaurantes.appendChild(restaurenteContainer);
+
+
+    });
 
 }
 
