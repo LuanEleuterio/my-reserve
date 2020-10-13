44e55aeb919795a8df36415f5d08dfdb015d2a9d
LuanEleuterio <luaneleuterio3@hotmail.com>
2020-10-13 19:42:50 -0300

    Adicionado inserção das reservs por JS, ajustado a responsividade de alguns elementos

--- src/pages/historico-reserva/historico-reserva.js
+++ src/pages/historico-reserva/historico-reserva.js
@@ -8,14 +8,12 @@ const rangeDistancia = document.querySelector('#range-distancia')
 const valueRangePessoa = document.querySelector('#value-range-pessoas')
 const valueRangeDistancia = document.querySelector('#value-range-distancia');
 const testeT = document.querySelector('#btn-edit-perfil')
-const categoriaMain = document.querySelector('.categorias-main')
 
 
-function carregaRestaurantes() {
-    const allRestaurantes = listaRestaurantes;
+function carregaReservas() {
+    const allReservas = listaReservas;
 
-    allRestaurantes.forEach(values => {
-        restaurenteContainer = document.createElement("a");
+    allReservas.forEach(values => {
         restaurantes = document.createElement("div");
 
         fotoRestaurante = document.createElement("div");
@@ -23,26 +21,50 @@ function carregaRestaurantes() {
 
         infoRestaurante = document.createElement("div");
         nomeRestaurante = document.createElement("p");
+        dataReserva = document.createElement("p");
+        hourReserva = document.createElement("p");
+        statusReserva = document.createElement("p");
 
-        restaurenteContainer.setAttribute("class", "restaurante-container");
+        buttonsOption = document.createElement("div")
+        btnVerRestaurante = document.createElement("button")
+        btnCancelar = document.createElement("button")
+
+        // restaurenteContainer.setAttribute("class", "restaurante-container");
         restaurantes.setAttribute("class", "restaurantes");
 
         fotoRestaurante.setAttribute("class", "foto-restaurante");
         imgRestaurante.setAttribute("src", values.img_url);
 
         infoRestaurante.setAttribute("class", "info-restaurante");
-        nomeRestaurante.setAttribute("class", "nome-restaurante infos");
-        nomeRestaurante.textContent = values.name;
+        nomeRestaurante.setAttribute("class", "nome-restaurante");
+        nomeRestaurante.textContent = values.name_restaurante;
+        dataReserva.setAttribute("class", "data-reserva col-info");
+        dataReserva.textContent = values.date_reserva;
+        hourReserva.setAttribute("class", "hour-reserva col-info");
+        hourReserva.textContent = values.hour_reserva;
+        statusReserva.setAttribute("class", "status-reserva col-info");
+        statusReserva.textContent = "Status: " + values.status_reserva;
 
+        buttonsOption.setAttribute("class", "btns-option")
+        btnVerRestaurante.setAttribute("class", "btn-ver-restaurante col-btn")
+        btnCancelar.setAttribute("class", "btn-cancelar col-btn")
+        btnVerRestaurante.textContent = "Visitar"
+        btnCancelar.textContent = "Cancelar"
 
-        restaurenteContainer.appendChild(restaurantes);
         restaurantes.appendChild(fotoRestaurante);
         fotoRestaurante.appendChild(imgRestaurante);
 
         restaurantes.appendChild(infoRestaurante);
         infoRestaurante.appendChild(nomeRestaurante);
+        infoRestaurante.appendChild(dataReserva);
+        infoRestaurante.appendChild(hourReserva);
+        infoRestaurante.appendChild(statusReserva);
 
-        conteinerRestaurantes.appendChild(restaurenteContainer);
+        restaurantes.appendChild(buttonsOption)
+        buttonsOption.appendChild(btnVerRestaurante)
+        if (values.status_reserva == "Reservado") { buttonsOption.appendChild(btnCancelar) }
+
+        conteinerRestaurantes.appendChild(restaurantes);
 
 
     });
@@ -70,7 +92,7 @@ function openModalPerfil() {
 
 btnPerfil.addEventListener("click", openModalPerfil)
 btnFilter.addEventListener("click", openModalFilter)
-//window.addEventListener("load", carregaRestaurantes)
+window.addEventListener("load", carregaReservas)
 
 
 window.addEventListener("load", () => {
