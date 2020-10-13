c785153fa3bb2695868aa725a17f84b9624fd294
LuanEleuterio <luaneleuterio3@hotmail.com>
2020-10-13 18:07:12 -0300

    First commit

---
+++ src/pages/historico-reserva/allReservas.js
@@ -0,0 +1,33 @@
+
+const listaReservas = [
+    {
+        id: 1,
+        reserva_id: 123456789,
+        day_reserva: "10/10/2020",
+        hour_reserva: "12:00 - 13:00",
+        status: "Concluído"
+    },
+    {
+        id: 2,
+        reserva_id: 123453789,
+        day_reserva: "13/10/2020",
+        hour_reserva: "15:00 - 16:00",
+        status: "Em Reserva"
+    }
+    ,
+    {
+        id: 3,
+        reserva_id: 122453789,
+        day_reserva: "01/10/2020",
+        hour_reserva: "11:00 - 12:30",
+        status: "Concluído"
+    }
+    ,
+    {
+        id: 4,
+        reserva_id: 123453759,
+        day_reserva: "17/09/2020",
+        hour_reserva: "12:00 - 13:00",
+        status: "Cancelado"
+    }
+]
--- src/pages/historico-reserva/historico-reserva.js
+++ src/pages/historico-reserva/historico-reserva.js
@@ -24,19 +24,6 @@ function carregaRestaurantes() {
         infoRestaurante = document.createElement("div");
         nomeRestaurante = document.createElement("p");
 
-        categoriaInfo = document.createElement("div");
-        iconCategoria = document.createElement("i");
-        nomeCategoria = document.createElement("p");
-
-        distanceInfo = document.createElement("div");
-        iconDistance = document.createElement("i");
-        distancia = document.createElement("p");
-
-        infoNumeroPessoas = document.createElement("div");
-        numeroPessoas = document.createElement("div");
-        qtdPessoas = document.createElement("p");
-        iconPessoas = document.createElement("i");
-
         restaurenteContainer.setAttribute("class", "restaurante-container");
         restaurantes.setAttribute("class", "restaurantes");
 
@@ -47,21 +34,6 @@ function carregaRestaurantes() {
         nomeRestaurante.setAttribute("class", "nome-restaurante infos");
         nomeRestaurante.textContent = values.name;
 
-        categoriaInfo.setAttribute("class", "categoria infos");
-        iconCategoria.setAttribute("class", "fas fa-utensils col-1");
-        nomeCategoria.setAttribute("class", "categoria")
-        nomeCategoria.textContent = values.category;
-
-        distanceInfo.setAttribute("class", "distance infos")
-        iconDistance.setAttribute("class", "fas fa-route col-1");
-        distancia.setAttribute("class", "distancia")
-        distancia.textContent = '2km';
-
-        infoNumeroPessoas.setAttribute("class", "info-num-pessoas");
-        numeroPessoas.setAttribute("class", "numero-pessoas");
-        qtdPessoas.setAttribute("class", "quantidade-pessoa");
-        iconPessoas.setAttribute("class", "fas fa-user-friends icon-pessoa");
-        qtdPessoas.textContent = values.totalPessoas;
 
         restaurenteContainer.appendChild(restaurantes);
         restaurantes.appendChild(fotoRestaurante);
@@ -69,58 +41,11 @@ function carregaRestaurantes() {
 
         restaurantes.appendChild(infoRestaurante);
         infoRestaurante.appendChild(nomeRestaurante);
-        infoRestaurante.appendChild(categoriaInfo);
-        categoriaInfo.appendChild(iconCategoria);
-        categoriaInfo.appendChild(nomeCategoria);
-
-        infoRestaurante.appendChild(distanceInfo);
-        distanceInfo.appendChild(iconDistance);
-        distanceInfo.appendChild(distancia);
-
-        restaurantes.appendChild(infoNumeroPessoas);
-        infoNumeroPessoas.appendChild(numeroPessoas);
-        numeroPessoas.appendChild(qtdPessoas);
-        numeroPessoas.appendChild(iconPessoas);
 
         conteinerRestaurantes.appendChild(restaurenteContainer);
 
 
     });
-
-}
-
-function carregaCategorias() {
-    const allCategorias = listaCategorias;
-    var scale = 'scale(1.5)';
-
-    btnPrev = document.createElement("button")
-
-    allCategorias.forEach(categoria => {
-        tipoCategoria = document.createElement("a");
-        imgCarrousel = document.createElement("figure");
-        imgCategoria = document.createElement("img");
-        nameCategoria = document.createElement("span");
-
-        tipoCategoria.setAttribute("class", "tipo-categoria")
-        imgCarrousel.setAttribute("class", "img-carrousel")
-        imgCategoria.setAttribute("class", "img-categoria")
-        nameCategoria.setAttribute("class", "name-categoria")
-        btnPrev.setAttribute("class", "slick-prev slick-arrow")
-
-        imgCategoria.setAttribute("src", categoria.img_url)
-        nameCategoria.textContent = categoria.name
-
-        tipoCategoria.appendChild(imgCarrousel)
-        imgCarrousel.appendChild(imgCategoria)
-        tipoCategoria.appendChild(nameCategoria)
-
-        categoriaMain.appendChild(tipoCategoria)
-
-    })
-
-
-
-
 
 }
 
@@ -145,8 +70,7 @@ function openModalPerfil() {
 
 btnPerfil.addEventListener("click", openModalPerfil)
 btnFilter.addEventListener("click", openModalFilter)
-window.addEventListener("load", carregaRestaurantes)
-window.addEventListener("load", carregaCategorias)
+//window.addEventListener("load", carregaRestaurantes)
 
 
 window.addEventListener("load", () => {
