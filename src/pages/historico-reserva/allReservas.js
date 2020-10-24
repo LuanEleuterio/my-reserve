1709271b8783511f28ee8a4b1cf35078c8290dc4
LuanEleuterio <luaneleuterio3@hotmail.com>
2020-10-23 22:15:56 -0300

    Ajustado o tamanho do modal de cancelamento e feito ajustes nos elementes dos restaurantes

--- src/pages/historico-reserva/allReservas.js
+++ src/pages/historico-reserva/allReservas.js
@@ -16,8 +16,25 @@ const listaReservas = [
         date_reserva: "13/10/2020",
         hour_reserva: "15:00 - 16:00",
         status_reserva: "Reservado"
-    }
-    ,
+    },
+    {
+        id: 2,
+        reserva_id: 123453789,
+        name_restaurante: "Madero",
+        img_url: "../../img/bar-1846137_1920.jpg",
+        date_reserva: "13/10/2020",
+        hour_reserva: "15:00 - 16:00",
+        status_reserva: "Reservado"
+    },
+    {
+        id: 2,
+        reserva_id: 123453789,
+        name_restaurante: "Madero",
+        img_url: "../../img/bar-1846137_1920.jpg",
+        date_reserva: "13/10/2020",
+        hour_reserva: "15:00 - 16:00",
+        status_reserva: "Reservado"
+    },
     {
         id: 3,
         reserva_id: 122453789,
--- src/pages/historico-reserva/historico-reserva.js
+++ src/pages/historico-reserva/historico-reserva.js
@@ -1,14 +1,4 @@
 const conteinerRestaurantes = document.querySelector('.conteudo-restaurantes')
-const modalFilter = document.querySelector('.modal-conteiner-filter')
-const modalPerfil = document.querySelector('.modal-conteiner-perfil')
-const btnFilter = document.querySelector('.btn-filter')
-const btnPerfil = document.querySelector('#btn-perfil')
-const rangePessoa = document.querySelector('#range-pessoa')
-const rangeDistancia = document.querySelector('#range-distancia')
-const valueRangePessoa = document.querySelector('#value-range-pessoas')
-const valueRangeDistancia = document.querySelector('#value-range-distancia');
-const testeT = document.querySelector('#btn-edit-perfil')
-
 
 function carregaReservas() {
     const allReservas = listaReservas;
@@ -68,6 +58,7 @@ function carregaReservas() {
 
 
     });
+
     /*Inicio modal cancelar*/
 
     const btnModalCancelar = document.getElementsByClassName("btn-cancelar")
@@ -75,69 +66,28 @@ function carregaReservas() {
 
     for (let i = 0; i < btnModalCancelar.length; i++) {
 
-        (function(index){
-            btnModalCancelar[index].addEventListener("click", function() {
-      
-            modalCancelar.classList.add("mostrar")
-      
-          })
+        (function (index) {
+            btnModalCancelar[index].addEventListener("click", function () {
+
+                modalCancelar.classList.add("mostrar")
+
+            })
         })(i)
-      }
-      
-      modalCancelar.addEventListener("click",function(e){
-      
-        if(e.target.className == modalCancelar.className || e.target.id == "button-fechar"){
+    }
+
+    modalCancelar.addEventListener("click", function (e) {
+
+        if (e.target.className == modalCancelar.className || e.target.id == "button-fechar") {
             modalCancelar.classList.remove("mostrar")
         }
-        
-      })
-      
+
+    })
+
     /*Fim modal cancelar*/
 }
 
-
-
-
-
-function openModalFilter() {
-    modalFilter.classList.add('mostrar')
-
-    modalFilter.addEventListener("click", (e) => {
-        if (e.target.id == "button-fechar" || e.target.className == modalFilter.className) {
-            modalFilter.classList.remove('mostrar')
-        }
-    })
-}
-
-
-function openModalPerfil() {
-    modalPerfil.classList.add('mostrar');
-
-    modalPerfil.addEventListener("click", (e) => {
-        console.log(e.target.className);
-        modalPerfil.classList.remove('mostrar')
-    })
-}
-
-
-btnPerfil.addEventListener("click", openModalPerfil)
-btnFilter.addEventListener("click", openModalFilter)
 window.addEventListener("load", carregaReservas)
 
-
-window.addEventListener("load", () => {
-    valueRangePessoa.textContent = rangePessoa.value;
-})
-window.addEventListener("load", () => {
-    valueRangeDistancia.textContent = rangeDistancia.value + 'km';
-})
-
-rangePessoa.addEventListener('change', (e) => {
-    valueRangePessoa.textContent = e.target.value;
-})
-rangeDistancia.addEventListener('change', (e) => {
-    valueRangeDistancia.textContent = e.target.value + 'km';
-})
 
 
 
