b6d3e589359a3377cd4db198a40f81084ae7b7ea
LuanEleuterio <luaneleuterio3@hotmail.com>
2020-10-24 20:00:08 -0300

    Adicionado o modal de cadastro e junto a ele a escolha do tipo de cadastro

--- src/pages/principal/principal.js
+++ src/pages/principal/principal.js
@@ -1,8 +1,22 @@
-const btnSign = document.querySelector("#btn-sign-up")
+var modalForm = document.querySelector('.modal-type-form')
+var btnSign = document.querySelector("#btn-sign-up")
+var noHaveAccount = document.querySelector("#no-have-account")
+
+
+function openModalTypeForm() {
+    console.log(modalForm)
+    modalForm.classList.add('mostrar')
+
+    modalForm.addEventListener("click", (e) => {
+        console.log(e)
+        if (e.target.className == modalForm.className) {
+            modalForm.classList.remove('mostrar')
+        }
+    })
+}
 
 
 
+btnSign.addEventListener("click", openModalTypeForm)
 
-btnSign.addEventListener("click", () => {
-    console.log("alo")
-})
+noHaveAccount.addEventListener("click", openModalTypeForm)
