54b9bfbe6a179036a86ff962473b583d6d21a2e6
Jessica-Parise <jparise.contato@hotmail.com>
2020-10-28 16:28:30 -0300

    Adding address modal not found

---
+++ src/pages/home/not-found.js
@@ -0,0 +1,19 @@
+// Pega o modal
+var modalNotFound = document.getElementById("myModal-not-found");
+
+// Pega o botão que irá abrir o modal
+var btnNotFound = document.getElementById("not-found");
+
+// Pega a tag que irá fechar o modal
+var spanCloseNf = document.getElementsByClassName("close-btnn-lcl")[0];
+
+// Quando clicar, abre o modal 
+btnNotFound.onclick = function () {
+  modalNotFound.style.display = "block";
+  modal.style.display = "none";
+}
+
+// Quando clicar, fecha o modal
+spanCloseNf.onclick = function () {
+  modalNotFound.style.display = "none";
+}
