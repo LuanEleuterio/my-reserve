797b50e9bc4c424d81002e63a270c77795c881e0
LuanEleuterio <luaneleuterio3@hotmail.com>
2020-10-29 15:15:31 -0300

    Ajustado o nome da variavel do button de horas

--- src/pages/info-horarios/info-horarios.js
+++ src/pages/info-horarios/info-horarios.js
@@ -3,7 +3,7 @@ const btnHourMobile = document.getElementsByClassName("btn-hour-mobile");
 for (let i = 0; i < btnHourMobile.length; i++) {
 
     (function (index) {
-        btn_horario[index].addEventListener("click", function () {
+        btnHourMobile[index].addEventListener("click", function () {
             window.location.href = '../confirmar-reserva/confirmar-reserva.html'
         })
     })(i)
