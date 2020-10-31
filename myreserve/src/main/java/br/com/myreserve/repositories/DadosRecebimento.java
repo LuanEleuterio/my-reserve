aee54ec9c5ce48e9a38009223a34e22caa48df2d
LuanEleuterio <luaneleuterio3@hotmail.com>
2020-10-31 14:22:27 -0300

    Corigido a nomenclatura do DadosRecebimento no Repositories

--- myreserve/src/main/java/br/com/myreserve/repositories/DadosRecebimento.java
+++
@@ -1,7 +0,0 @@
-package br.com.myreserve.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface DadosRecebimento extends CrudRepository<DadosRecebimento, Integer>{
-
-}
---
+++ myreserve/src/main/java/br/com/myreserve/repositories/DadosRecebimentoRepository.java
@@ -0,0 +1,7 @@
+package br.com.myreserve.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface DadosRecebimentoRepository extends CrudRepository<DadosRecebimentoRepository, Integer>{
+
+}
