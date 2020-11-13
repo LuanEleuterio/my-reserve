d830ce04ed012b8633d5bba227e0e031feab82d4
Ellisys <gustavo_falv@hotmail.com>
2020-11-13 16:02:03 -0300

    Adicionado controller upload de imagem

---
+++ myreserve/src/main/java/br/com/myreserve/utils/UploadUtil.java
@@ -0,0 +1,27 @@
+package br.com.myreserve.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class UploadUtil {
+	public static void saveFile(String uploadDir , String fileName, MultipartFile file) throws IOException {
+		Path uploadPath = Paths.get(uploadDir);
+		
+		if(!Files.exists(uploadPath)) {
+			Files.createDirectories(uploadPath);
+		}
+		
+		try (InputStream inputStream = file.getInputStream()){
+			Path filePath = uploadPath.resolve(fileName);
+			Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+		}catch (IOException e) {
+			throw new IOException("Nao foi possivel enviar o arquivo "+ fileName , e);
+		}
+	}
+}
