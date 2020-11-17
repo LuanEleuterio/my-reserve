813365d2c08e3462673eeb932bd70e8bfc5c9cd3
Jessica-Parise <jparise.contato@hotmail.com>
2020-11-17 18:32:58 -0300

    Adding mask for the 'CEP'

--- src/pages/cadastro-empresa/cad-empresa.js
+++ src/pages/cadastro-empresa/cad-empresa.js
@@ -22,6 +22,7 @@ var bodyDadosEndereco = {}
 
 const btnSubmit = document.getElementById("submit-estab")
 
+$("#cep").mask("00000-000");
 $("#telefone_um, #telefone_dois").mask("(00) 00000-0000");
 $("#cnpj").mask("00.000.000/0000-00");
 
