7ef42fdcc9b77f29b3948dcd553c5bd8bbb2ef86
LuanEleuterio <luaneleuterio3@hotmail.com>
2020-11-18 15:58:44 -0300

    Recolocado alterações perdidadas no ultimo commit

--- src/utils/not-found.js
+++ src/utils/not-found.js
@@ -1,19 +1,38 @@
 // Pega o modal
-var modalNotFound = document.getElementById("myModal-not-found");
+const modalNotFound = document.getElementById("myModal-not-found");
+const modalLocation = document.querySelector(".model-conteiner-lcl");
+const btnNotFound = document.getElementById("myBtn-loc");
+const cepAtNow = document.getElementById("cep")
+const logradouroLoc = document.getElementById("logradouro")
+const cidadeLoc = document.getElementById("cidade")
+const numeroLoc = document.getElementById("numero")
+const enderecoAtNow = document.getElementById("seu-endereco")
+const btnSubmitLoc = document.getElementById("submit-location")
 
-// Pega o botão que irá abrir o modal
-var btnNotFound = document.getElementById("myBtn-loc");
-
-// Pega a tag que irá fechar o modal
-var spanCloseNf = document.getElementsByClassName("close-btnn-lcl")[0];
-
-// Quando clicar, abre o modal 
-btnNotFound.onclick = function () {
-  modalNotFound.style.display = "flex";
-  modal.style.display = "none";
+function openModalLocation() {
+  modalNotFound.classList.add('mostrar')
+  console.log(cepAtNow)
+  let anyLocVazio = (cepAtNow.value == "" && logradouroLoc.value == "" && cidadeLoc.value == "" && numeroLoc.value == "") ? false : true
+  modalNotFound.addEventListener("click", (e) => {
+    if ((e.target.className == "close-btnn-lcl" || e.target.className == modalNotFound.className) && anyLocVazio) {
+      modalNotFound.classList.remove('mostrar')
+    }
+  })
 }
 
-// Quando clicar, fecha o modal
-spanCloseNf.onclick = function () {
-  modalNotFound.style.display = "none";
-}
+btnNotFound.addEventListener("click", openModalLocation)
+
+btnSubmitLoc.addEventListener("click", (e) => {
+  e.preventDefault()
+
+  console.log(modalNotFound)
+  enderecoAtNow.textContent = `${logradouroLoc.value}, ${numeroLoc.value}`
+
+  modalNotFound.classList.remove('mostrar')
+})
+
+window.addEventListener("load", () => {
+  if (cepAtNow.value === "") {
+    openModalLocation()
+  }
+})
