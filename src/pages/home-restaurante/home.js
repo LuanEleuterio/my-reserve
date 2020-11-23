f68e43f4eff9a91fe1ea486571b137bfffaab60a
LuanEleuterio <luaneleuterio3@hotmail.com>
2020-11-22 22:20:28 -0300

    Feito a integração da pagina Perfil do restaurante no mobile

--- src/pages/home-restaurante/home.js
+++ src/pages/home-restaurante/home.js
@@ -10,19 +10,19 @@ var dataDeHoje = new Date()
 
 
 function carregaClientes() {
-    fetch("http://localhost:8080/reserva")
-    .then( (res)=> res.json())
-    .then( (data)=> {
-    const reservas = data
+  fetch("http://localhost:8080/reserva")
+    .then((res) => res.json())
+    .then((data) => {
+      const reservas = data
 
 
-    const reservasDoDia = reservas.filter((obj) => {
-      return dataDeHoje.getDate() == obj.data_reserva.slice(-2,10) && dataDeHoje.getMonth()+1 == obj.data_reserva.slice(5,-3)
-    }) 
+      const reservasDoDia = reservas.filter((obj) => {
+        return dataDeHoje.getDate() == obj.data_reserva.slice(-2, 10) && dataDeHoje.getMonth() + 1 == obj.data_reserva.slice(5, -3)
+      })
 
-    reservasDoDia.forEach(values => {
-        
-        
+      reservasDoDia.forEach(values => {
+
+
         clienteContainer = document.createElement("a");
         clientes = document.createElement("div");
 
@@ -67,7 +67,7 @@ function carregaClientes() {
         hourInfo.setAttribute("class", "hourInfo infos")
         iconRelogio.setAttribute("class", "far fa-clock col-1");
         hourText.setAttribute("class", "horario")
-        hourText.textContent = values.horario.horario_de.slice(-8,-3);
+        hourText.textContent = values.horario.horario_de.slice(-8, -3);
 
         containerIcons.setAttribute("class", "container-icons");
         numeroPessoas.setAttribute("class", "numero-pessoas");
@@ -103,36 +103,36 @@ function carregaClientes() {
         containerClientes.appendChild(clienteContainer);
 
       })
-    
+
       const blockClientes = document.getElementsByClassName("clientes");
       for (let i = 0; i < blockClientes.length; i++) {
 
         (function (index) {
-            blockClientes[index].addEventListener("click", function () {
+          blockClientes[index].addEventListener("click", function () {
             modalCancel.style.display = "flex"
             modalBtnSubmit.disabled = true
             modalBtnSubmit.style.background = "#CCC"
- 
-              
-           id_justifica = blockClientes[index].attributes[1].value
 
 
-              pDataReserva.innerText = FormataStringData(data[index].data_reserva)
-             pHoraReserva.innerText = data[index].horario.horario_de.slice(-8,-3) +" às "+ data[index].horario.horario_ate.slice(-8,-3)
-            
+            id_justifica = blockClientes[index].attributes[1].value
+
+
+            pDataReserva.innerText = FormataStringData(data[index].data_reserva)
+            pHoraReserva.innerText = data[index].horario.horario_de.slice(-8, -3) + " às " + data[index].horario.horario_ate.slice(-8, -3)
+
           })
         })(i)
       }
-    
+
     });
-    
+
 }
 function FormataStringData(d) {
-  var dia  = d.split("-")[0];
-  var mes  = d.split("-")[1];
-  var ano  = d.split("-")[2];
+  var dia = d.split("-")[0];
+  var mes = d.split("-")[1];
+  var ano = d.split("-")[2];
 
-  return ano + '-' + ("0"+mes).slice(-2) + '-' + ("0"+dia).slice(-2);
+  return ano + '-' + ("0" + mes).slice(-2) + '-' + ("0" + dia).slice(-2);
 }
 
 const modalCancel = document.getElementById("myModal-cancel");
@@ -140,57 +140,57 @@ const modalBtnSubmit = document.getElementById("btnSubmitCancelar")
 const txtModalCancel = document.getElementById("txtModalCancel")
 
 modalCancel.addEventListener("click", function (e) {
-    if (e.target.style.display == "flex" || e.target.id == "btn-cross-modal") {
-      modalCancel.style.display = "none";
-      txtModalCancel.value = ""
-    }
-  })
-
-txtModalCancel.addEventListener("input",function(){
-
-        modalBtnSubmit.disabled = false
-        modalBtnSubmit.style.background = "#4361EE"
-        if(txtModalCancel.value == ""){
-            modalBtnSubmit.disabled = true
-            modalBtnSubmit.style.background = "#CCC"
-        }
+  if (e.target.style.display == "flex" || e.target.id == "btn-cross-modal") {
+    modalCancel.style.display = "none";
+    txtModalCancel.value = ""
+  }
 })
 
-modalBtnSubmit.addEventListener("click",function(){
-    if(txtModalCancel.value == ""){
-        
+txtModalCancel.addEventListener("input", function () {
+
+  modalBtnSubmit.disabled = false
+  modalBtnSubmit.style.background = "#4361EE"
+  if (txtModalCancel.value == "") {
+    modalBtnSubmit.disabled = true
+    modalBtnSubmit.style.background = "#CCC"
+  }
+})
+
+modalBtnSubmit.addEventListener("click", function () {
+  if (txtModalCancel.value == "") {
+
+  }
+  if (txtModalCancel.value != "") {
+    const objteste = {
+      fk_reserva: parseInt(id_justifica),
+      justificativa: txtModalCancel.value
     }
-    if(txtModalCancel.value != ""){
-      const objteste = {
-        fk_reserva: parseInt(id_justifica),
-        justificativa: txtModalCancel.value
-      }
 
 
-      fetch("http://localhost:8080/cancel-justifica",{
-        method: "POST",
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(objteste)
-      })
-      .then(res =>{
-        if(!res.ok){
+    fetch("http://localhost:8080/cancel-justifica", {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(objteste)
+    })
+      .then(res => {
+        if (!res.ok) {
           throw Error(res.statusText)
-        }else{
+        } else {
           return res.json()
         }
       })
       .then(okCancel => {
-          if(okCancel){
+        if (okCancel) {
           exibeAlert(true)
-        }else{
+        } else {
           exibeAlert(false)
         }
       })
-      .catch(err=> exibeAlert(false))
-    }
+      .catch(err => exibeAlert(false))
+  }
 })
 
 function exibeAlert(exibe) {
@@ -213,70 +213,70 @@ function exibeAlert(exibe) {
 
 
 fetch("http://localhost:8080/categoria")
-      .then( (res)=> res.json())
-      .then((data) =>{
-        const categoriaRestaurante = data
+  .then((res) => res.json())
+  .then((data) => {
+    const categoriaRestaurante = data
 
-        categoriaRestaurante.forEach(tipos =>{
-          opt = document.createElement('option');
-          opt.value = tipos.id_categoria
-          opt.innerHTML = tipos.tipo_categoria;
-          opt.setAttribute("data-values",tipos.id_categoria)
-          modalCategoria.appendChild(opt)
-
-
-
-        })
-        
-      })
-
-      fetch(`http://localhost:8080/restaurante/${localStorage.getItem('myreserve-usr-identifier')}`)
-      .then((res)=> res.json())
-      .then((data) => {
-
-       
-        document.getElementById("categoria-option").value = data.categoria.id_categoria
-
-        document.getElementById("name").value = data.nome
-        document.getElementById("cnpj").value = data.cnpj
-        document.getElementById("email").value = data.email
-
-        document.getElementById("horarioDe").value = data.hora_funcionamento.slice(0,5)
-        document.getElementById("horarioAte").value = data.hora_funcionamento.slice(-5)
+    categoriaRestaurante.forEach(tipos => {
+      opt = document.createElement('option');
+      opt.value = tipos.id_categoria
+      opt.innerHTML = tipos.tipo_categoria;
+      opt.setAttribute("data-values", tipos.id_categoria)
+      modalCategoria.appendChild(opt)
 
 
-      })
 
-btnModalEstab.addEventListener("click",()=>{
+    })
+
+  })
+
+fetch(`http://localhost:8080/restaurante/${localStorage.getItem('myreserve-usr-identifier')}`)
+  .then((res) => res.json())
+  .then((data) => {
+
+
+    document.getElementById("categoria-option").value = data.categoria.id_categoria
+
+    document.getElementById("name").value = data.nome
+    document.getElementById("cnpj").value = data.cnpj
+    document.getElementById("email").value = data.email
+
+    document.getElementById("horarioDe").value = data.hora_funcionamento.slice(0, 5)
+    document.getElementById("horarioAte").value = data.hora_funcionamento.slice(-5)
+
+
+  })
+
+btnModalEstab.addEventListener("click", (event) => {
   event.preventDefault()
   let objEstab = {}
 
-  if(document.getElementById("password").value == ""){
- 
-  objEstab = {
-    fk_categoria:  document.getElementById("categoria-option").value,
-    nome: document.getElementById("name").value,
-    cnpj: document.getElementById("cnpj").value,
-    email: document.getElementById("email").value,
-    hora_funcionamento: document.getElementById("horarioDe").value +" às "+ document.getElementById("horarioAte").value,
-    descricao: document.getElementById("story").value
+  if (document.getElementById("password").value == "") {
+
+    objEstab = {
+      fk_categoria: document.getElementById("categoria-option").value,
+      nome: document.getElementById("name").value,
+      cnpj: document.getElementById("cnpj").value,
+      email: document.getElementById("email").value,
+      hora_funcionamento: document.getElementById("horarioDe").value + " às " + document.getElementById("horarioAte").value,
+      descricao: document.getElementById("story").value
     }
   }
-  if(document.getElementById("password").value != ""){
- 
+  if (document.getElementById("password").value != "") {
+
     objEstab = {
-      fk_categoria:  document.getElementById("categoria-option").value,
+      fk_categoria: document.getElementById("categoria-option").value,
       nome: document.getElementById("name").value,
       cnpj: document.getElementById("cnpj").value,
       email: document.getElementById("email").value,
       senha: document.getElementById("password").value,
-      hora_funcionamento: document.getElementById("horarioDe").value +" às "+ document.getElementById("horarioAte").value,
+      hora_funcionamento: document.getElementById("horarioDe").value + " às " + document.getElementById("horarioAte").value,
       descricao: document.getElementById("story").value
-      }
+    }
   }
- 
-  
-  fetch(`http://localhost:8080/restaurante/${localStorage.getItem('myreserve-usr-identifier')}`,{
+
+
+  fetch(`http://localhost:8080/restaurante/${localStorage.getItem('myreserve-usr-identifier')}`, {
     method: "PUT",
     headers: {
       'Accept': 'application/json',
@@ -284,12 +284,12 @@ btnModalEstab.addEventListener("click",()=>{
     },
     body: JSON.stringify(objEstab)
   })
-  
+
 })
 
 
 window.addEventListener("load", carregaClientes)
 
 configHourRedirect.addEventListener("click", () => {
-    window.location.href = "../configuracao-horario/configuracao-horario.html"
+  window.location.href = "../configuracao-horario/configuracao-horario.html"
 })
--- src/pages/restaurante-perfil/senha.js
+++ src/pages/restaurante-perfil/senha.js
@@ -1,3 +1,6 @@
+const modalCategoria = document.getElementById("categoria-option")
+const btnModalEstab = document.getElementById("submit-salvar")
+
 // mostrar senha - inicio
 function mostrar(e) {
   var tipo = e.parentNode.querySelector("[name='senha']");
@@ -18,3 +21,106 @@ function mostrar(e) {
   }
 }
 // mostrar senha - fim
+
+function carregaDados() {
+  fetch("http://localhost:8080/categoria")
+    .then((res) => res.json())
+    .then((data) => {
+      const categoriaRestaurante = data
+
+      categoriaRestaurante.forEach(tipos => {
+        opt = document.createElement('option');
+        opt.value = tipos.id_categoria
+        opt.innerHTML = tipos.tipo_categoria;
+        opt.setAttribute("data-values", tipos.id_categoria)
+        modalCategoria.appendChild(opt)
+      })
+
+    })
+
+  fetch(`http://localhost:8080/restaurante/${localStorage.getItem('myreserve-usr-identifier')}`)
+    .then((res) => res.json())
+    .then((data) => {
+
+      document.getElementById("foto-restaurante").setAttribute("src", "../../../myreserve/" + data.img_estabelecimento)
+      document.getElementById("categoria-option").value = data.categoria.id_categoria
+
+      document.getElementById("name").value = data.nome
+      document.getElementById("cnpj").value = data.cnpj
+      document.getElementById("email").value = data.email
+      document.getElementById("story").value = data.descricao
+      document.getElementById("horarioDe").value = data.hora_funcionamento.slice(0, 5)
+      document.getElementById("horarioAte").value = data.hora_funcionamento.slice(-5)
+
+
+    })
+}
+
+
+btnModalEstab.addEventListener("click", (event) => {
+  event.preventDefault()
+  let objEstab = {}
+
+  if (document.getElementById("password").value == "") {
+
+    objEstab = {
+      fk_categoria: parseInt(document.getElementById("categoria-option").value),
+      nome: document.getElementById("name").value,
+      cnpj: document.getElementById("cnpj").value,
+      email: document.getElementById("email").value,
+      hora_funcionamento: document.getElementById("horarioDe").value + " às " + document.getElementById("horarioAte").value,
+      descricao: document.getElementById("story").value
+    }
+  }
+  if (document.getElementById("password").value != "") {
+
+    objEstab = {
+      fk_categoria: parseInt(document.getElementById("categoria-option").value),
+      nome: document.getElementById("name").value,
+      cnpj: document.getElementById("cnpj").value,
+      email: document.getElementById("email").value,
+      senha: document.getElementById("password").value,
+      hora_funcionamento: document.getElementById("horarioDe").value + " às " + document.getElementById("horarioAte").value,
+      descricao: document.getElementById("story").value
+    }
+  }
+
+  fetch(`http://localhost:8080/restaurante/${localStorage.getItem('myreserve-usr-identifier')}`, {
+    method: "PUT",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(objEstab)
+  }).then(res => {
+    if (!res.ok) {
+      throw Error(res.statusText)
+    } else {
+      exibeAlertMobileRest(true)
+      return res.json()
+    }
+  }).catch(err => {
+    exibeAlertMobileRest(false)
+    console.log(err)
+  })
+})
+
+window.addEventListener("load", carregaDados)
+
+function exibeAlertMobileRest(exibe) {
+  if (exibe) {
+    Swal.fire({
+      icon: 'success',
+      title: 'Os dados foram alterados',
+      showConfirmButton: false,
+      timer: 3500,
+    })
+  } else {
+    Swal.fire({
+      icon: 'error',
+      title: 'Não foi possível alterar os dados',
+      showConfirmButton: false,
+      timer: 2500,
+    })
+  }
+}
