ff5dab5f2dedace63d886ac4c23794a3e694f50d
Jessica-Parise <jparise.contato@hotmail.com>
2020-11-23 23:11:44 -0300

    Creating Category 'Put'

--- myreserve/src/main/java/br/com/myreserve/controllers/CategoriaController.java
+++ myreserve/src/main/java/br/com/myreserve/controllers/CategoriaController.java
@@ -7,11 +7,13 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.myreserve.entities.Categoria;
+import br.com.myreserve.entities.Telefone;
 import br.com.myreserve.repositories.CategoriaRepository;
 
 @RestController
@@ -35,5 +37,17 @@ public class CategoriaController {
 	@PostMapping()
 	public void addCategoria(@RequestBody Categoria categoria) {
 		categoriaRepository.save(categoria);
+	}
+	
+	@CrossOrigin
+	@PutMapping("/{idCategoria}")
+	public Categoria updateCategoria(@PathVariable Integer idCategoria, @RequestBody Categoria dadosCategoria) throws Exception{
+		Categoria categoriaDB = categoriaRepository.findById(idCategoria)
+				.orElseThrow(() -> new IllegalAccessException());
+		
+		if(dadosCategoria.getTipo_categoria() != null) categoriaDB.setTipo_categoria(dadosCategoria.getTipo_categoria());
+		if(dadosCategoria.getImg_categoria() != null) categoriaDB.setImg_categoria(dadosCategoria.getImg_categoria());
+		
+		return categoriaRepository.save(categoriaDB);
 	}
 }
