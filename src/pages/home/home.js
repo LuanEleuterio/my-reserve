682aed91a9ad4685dac2c963f7558f6663a353e0
LuanEleuterio <luaneleuterio3@hotmail.com>
2020-11-25 17:45:30 -0300

    Colocado a chamada do slick carrousel na home.js, para assim ele ser montado somente após o termino da requisição das categorias

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -215,12 +215,81 @@ window.addEventListener("load", () => {
                 })(i)
             }
 
+            montaCarrouselSlick()
         })
         .catch(err => console.log(err))
 
 
 })
-//-
+
+function montaCarrouselSlick() {
+
+    $('.categorias-main').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 7,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 7,
+                    infinite: true,
+                    slidesToScroll: 1,
+                    infinite: true
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 7,
+                    infinite: true,
+                    slidesToScroll: 1,
+                    infinite: true
+                }
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 7,
+                    infinite: true,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 1250,
+                settings: {
+                    slidesToShow: 7,
+                    infinite: true,
+                    slidesToScroll: 1
+                }
+            },
+            ,
+            {
+                breakpoint: 1300,
+                settings: {
+                    slidesToShow: 7,
+                    infinite: true,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 1365,
+                settings: {
+                    slidesToShow: 7,
+                    infinite: true,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+}
 
 btnVerMais.addEventListener("click", () => {
     proxPage++
